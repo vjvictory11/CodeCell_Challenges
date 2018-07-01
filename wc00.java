c22b5a64ff2523e761ef3bec0b6b678c063a2116
Vijay_Rajput <vj898368@gmail.com>
2018-07-01 20:38:26 +0530

    donw wc00

---
+++ wc00.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class wc00 {
+    public static void main(String args[]) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int f = (n*(n+1))/2;
+        int s = (10*n*(10*n+1))/2;
+        int t = (20*n*(20*n+1))/2;
+        System.out.println(f+"+"+s+"+"+t);
+    }
+}
+
+/*this is the output*/
