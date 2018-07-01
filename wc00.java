6ae8679daad3d31d6823d009da106705a479dbe0
vjvictory11 <26359315+vjvictory11@users.noreply.github.com>
2018-07-02 01:50:39 +0530

    Update wc00.java

--- wc00.java
+++ wc00.java
@@ -8,6 +8,7 @@ public class wc00 {
         int f = (n*(n+1))/2;
         int s = (10*n*(10*n+1))/2;
         int t = (20*n*(20*n+1))/2;
-        System.out.println(f+"+"+s+"+"+t);
+//         System.out.println(f+"+"+s+"+"+t);
+        System.out.println(f+s+t);
     }
 }
