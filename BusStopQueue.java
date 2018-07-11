6ed7eb8e509f8c95ffcd6cebfca6087567f20a4d
vjvictory11 <26359315+vjvictory11@users.noreply.github.com>
2018-07-12 00:34:41 +0530

    Update BusStopQueue.java

--- BusStopQueue.java
+++ BusStopQueue.java
@@ -9,16 +9,36 @@ public class BusStopQueue {
         int ans = 0;
         for (int i = 0;i< t; i++) {
             int s = sc.nextInt();
+//            System.out.println("S value "+s);
+//            System.out.println("enter no. of items");
             int n = sc.nextInt();
-            int total =0;
             int[] arr = new int[n];
             for(int j=0;j<n;j++) {
                 arr[j] = sc.nextInt();
-                total+=arr[j];
             }
-            ans += (int)Math.ceil((double)total/s);
-            System.out.println(ans);
+            int index = 0;
+            int sum = 0;
+            int count = 0;
+            while(index<arr.length) {
+              if(arr[index] > s) {
+                  arr[index] -= s;
+                  count ++;
+//                  System.out.println("vale of count "+count);
+              }
+              else {
+                  while(index<arr.length && arr[index]+sum <= s) {
+                      sum +=arr[index];
+                      index++;
+                  }
+                  count ++;
+//                  System.out.println("vale of count "+count);
+                  sum = 0;
+              }
+            }
+            System.out.println(count);
+            ans +=count;
         }
+        System.out.println(ans);
 
     }
 }
